14491e8babcca10a0166f1577b17b9a2ecc044cf
Ngô Lê Gia Bảo <52200053@student.tdtu.edu.vn>
2025-02-13 21:41:59 +0700

    Feat: Connecting  to SQL Server for Route and Trip Management Service

---
+++ back-end/route-and-trip-management-service/route-and-trip-management-service/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace route_and_trip_management_service.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ back-end/route-and-trip-management-service/route-and-trip-management-service/Program.cs
@@ -0,0 +1,48 @@
+
+using Microsoft.EntityFrameworkCore;
+using route_and_trip_management_service.Models;
+
+namespace route_and_trip_management_service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            // Register service AppDbContext
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                string connectionString = builder.Configuration.GetConnectionString("DefaultConectionString");
+                options.UseSqlServer(connectionString);
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
